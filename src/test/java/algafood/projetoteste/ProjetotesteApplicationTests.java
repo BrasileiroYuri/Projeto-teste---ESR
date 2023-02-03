b353035bd09defea07d498c7cb5b7506ce402d98
Yuri Santos <yurimax322@gmail.com>
2023-02-03 18:18:02 -0300

    Versao inicial do projeto.

---
+++ src/test/java/algafood/projetoteste/ProjetotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package algafood.projetoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
