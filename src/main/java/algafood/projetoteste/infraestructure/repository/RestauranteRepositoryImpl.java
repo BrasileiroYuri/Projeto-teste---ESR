8e3f7cf4f79343a7931beab56ccfd3cb41d0f387
Yuri Santos <yurimax322@gmail.com>
2023-02-12 11:58:12 -0300

    Refatorando Query para retornar String.

--- src/main/java/algafood/projetoteste/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/algafood/projetoteste/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -24,12 +24,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     public List<Restaurante> findB(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
         var parametros = new HashMap<String, Object>();
         var jpql = getJpql(nome, parametros, taxaFreteFinal, taxaFreteInicial);
-        TypedQuery<Restaurante> typedQuery = entityManager.createQuery(jpql.toString(), Restaurante.class);
+        TypedQuery<Restaurante> typedQuery = entityManager.createQuery(jpql, Restaurante.class);
         parametros.forEach(typedQuery::setParameter);
         return typedQuery.getResultList();
     }
 
-    private static StringBuilder getJpql(String nome, HashMap<String, Object> paramentos,
+    private String getJpql(String nome, HashMap<String, Object> paramentos,
                                          BigDecimal taxaFreteFinal, BigDecimal taxaFreteInicial) {
         var jpql = new StringBuilder();
         jpql.append("from Restaurante where 0 = 0 ");
@@ -45,7 +45,7 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
             jpql.append("and taxaFrete <= :taxaFinal");
             paramentos.put("taxaFinal", taxaFreteFinal);
         }
-        return jpql;
+        return jpql.toString();
     }
 
     public List<Restaurante> listar() {
